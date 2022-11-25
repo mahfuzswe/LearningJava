a3b2bd7a0d47fda767a9cfc7496a2e06a515be82
Invincible-21 <shantobd1969@gmail.com>
2022-11-25 23:58:06 +0600

    good night

---
+++ 6.Variables and Data types/Comments.java
@@ -0,0 +1,19 @@
+public class Comments {
+    public static void main(String args[]) {
+        // code to calculate sum
+        /*
+         code to
+         calculate 
+         sum
+         comments
+         hijibiji
+         hijibiji
+         .....
+         */
+
+        int a = 10;
+        int b = 5;
+        int sum = a + b;
+        System.out.println(sum);
+    }
+}
