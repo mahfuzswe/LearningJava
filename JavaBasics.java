3a7468c7ba58183464ec2598b8ae665eac89b60e
Invincible-21 <shantobd1969@gmail.com>
2022-11-25 21:19:08 +0600

    initial commit

---
+++ JavaBasics.java
@@ -0,0 +1,11 @@
+public class JavaBasics {
+    public static void main(String args[]){
+        System.out.print("Hello, World!\n"); //line ka space
+        System.out.print("Hello, World!\n"); //Line ka space
+        System.out.println("Hello, World!"); //line ka space
+
+    }
+}
+
+//boilerplate code
+
