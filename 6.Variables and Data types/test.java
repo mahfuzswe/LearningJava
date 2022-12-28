841da8dd9b710f707c97f43f92f936922c9d03b0
Invincible-21 <shantobd1969@gmail.com>
2022-12-28 22:01:43 +0600

    updated

--- 6.Variables and Data types/test.java
+++ 6.Variables and Data types/test.java
@@ -1,5 +1,20 @@
+import java.util.Scanner;
+
 public class test{
     public static void main(String args[]){
-        
+        Scanner sc = new Scanner(System.in);
+
+        // String name = sc.nextLine();
+        // System.out.println(name);
+
+        // float a = sc.nextFloat();
+        // System.out.println(a);
+
+        // double d = sc.nextDouble();
+        // System.out.println(d);
+
+        long a = sc.nextLong();
+        System.out.println(a);
+
     }
 }
