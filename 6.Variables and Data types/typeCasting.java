241cc91b48bd09b49766b72bf737f19321653e20
Invincible-21 <shantobd1969@gmail.com>
2022-12-29 00:50:00 +0600

    updated

---
+++ 6.Variables and Data types/typeCasting.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class typeCasting {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+
+        // float a = 25.1299f;
+        // int b = (int)a;
+        // System.out.println(b);
+
+        // float number = 99.9999f;
+        // int number2 = (int)number;
+        // System.out.println(number2);
+
+        char ch = 'a';
+        char ch2 = 'b';
+        int number = ch;
+        int number2 = ch2;
+        System.out.println(number); 
+        System.out.println(number2); 
+    }
+}
+
+
+// type casting also called : narrowing coversion, explicit conversion
