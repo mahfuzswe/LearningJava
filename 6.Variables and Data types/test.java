e72fb6c33f008ddddb0e4c8daf3cd5d0fb746f69
Invincible-21 <shantobd1969@gmail.com>
2022-12-28 20:03:11 +0600

    updated

--- 6.Variables and Data types/test.java
+++ 6.Variables and Data types/test.java
@@ -1,9 +1,5 @@
 public class test{
     public static void main(String args[]){
-        System.out.println("\n\n****");
-        System.out.println("***");
-        System.out.println("**");
-        System.out.println("*\n\n");
-
+        
     }
 }
