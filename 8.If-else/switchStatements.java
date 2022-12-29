ec922d4dbd1f711a8db5c8035e2f13a15d01f9f0
Invincible-21 <shantobd1969@gmail.com>
2022-12-30 00:28:04 +0600

    updated

---
+++ 8.If-else/switchStatements.java
@@ -0,0 +1,16 @@
+import javax.swing.text.html.StyleSheet;
+
+public class switchStatements {
+    public static void main(String args[]){
+        int number = 3;
+
+        switch (number) {
+            case 1: System.out.println("somosa");
+                break;
+            case 2: System.out.println("burger");
+                break;
+            default: System.out.println("I realize I'm dreaming"); 
+                break;
+        }
+    }
+}
