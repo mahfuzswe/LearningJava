ca927b698c1272c851d0b630b17c3f8d6d0c5467
Invincible-21 <shantobd1969@gmail.com>
2022-12-29 13:13:26 +0600

    updated

---
+++ 7.Operators/logicalOperates.java
@@ -0,0 +1,15 @@
+public class logicalOperates {
+    public static void main(String args[]){
+        // Logical Operates:
+        // 1. && -> and operator ; true iff both true
+        // 2. || (double pipe) -> or operator ; true iff one true
+        // 3. ! -> not operator ; !(true) -> false ; !(false) -> true
+
+        // System.out.println((5>0) && (0>1));
+        // System.out.println((5>0) || (0>1));
+        // System.out.println(!(5>0));
+        System.out.println(!(5<0));
+
+
+     }
+}
