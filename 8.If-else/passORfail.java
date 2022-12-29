b8bcc285473dfbc669929eb385b62524c0598200
Invincible-21 <shantobd1969@gmail.com>
2022-12-30 00:13:56 +0600

    updated

---
+++ 8.If-else/passORfail.java
@@ -0,0 +1,8 @@
+public class passORfail {
+    public static void main(String args[]){
+        int marks = 80;
+
+        String reportCard = marks >= 33 ? "PASS" : "FAIL";
+        System.out.println(reportCard);
+    }
+}
