1ab951060dfb17a479a63909333d6df03644d4fe
Invincible-21 <shantobd1969@gmail.com>
2023-01-23 01:27:15 +0600

    updated

---
+++ 17.Bit Manipulation/ithBit.java
@@ -0,0 +1,16 @@
+public class ithBit {
+    
+    public static int getIthBit(int n, int i){
+        int bitMask = 1<<i;
+        if((n & bitMask) == 0){
+            return 0;
+        }else{
+            return 1;
+        }
+    }
+
+
+    public static void main(String args[]){
+        System.out.print(getIthBit(10, 3));
+    }
+}
