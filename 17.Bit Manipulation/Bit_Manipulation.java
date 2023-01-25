694fb02dfb362c54fdabd85c61d4bb54e16db36a
Invincible-21 <shantobd1969@gmail.com>
2023-01-25 13:55:25 +0600

    updated

---
+++ 17.Bit Manipulation/Bit_Manipulation.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Bit_Manipulation {
+    
+    public static void oddOrEven(int n){
+        int bitMask = 1;
+        if((n & bitMask)==0){
+            System.out.println("Even number");
+        }else{
+            System.out.println("Odd number");
+        }
+    }
+
+    public static void main(String args[]){
+        oddOrEven(5);
+    }
+}
